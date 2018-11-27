ba3a70cffe269603c4af58f535f939d8a2584cff
yangjeehan <yangjeehan@naver.com>
2018-11-27 23:01:03 +0900

    add SingleThreadBlocking Server

---
+++ NonBlockingServer/src/com/yjh/MultiThreadedBlockingServer.java
@@ -0,0 +1,38 @@
+package com.yjh;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class MultiThreadedBlockingServer {
+	
+	public static void main(String[] args) throws IOException {
+		ServerSocket ss = new ServerSocket(8080);
+		while(true) {
+			Socket s = ss.accept();
+			handle(s);
+		}
+	
+	}
+
+	private static void handle(Socket s) throws IOException {
+		System.out.println("Connected to " + s);
+		try (
+			Socket temp = s;
+			InputStream in = s.getInputStream();
+			OutputStream out = s.getOutputStream();
+		)   {
+			int data;
+			while((data = in.read()) != -1 ) {
+				out.write(transmogrify(data));
+			}
+		} finally {
+			System.out.println("Disconnected from " + s );
+		}
+	}
+	
+	private static int transmogrify(int data) {
+		return Character.isLetter(data) ? data ^ ' ' : data;
+	}
+}
---
+++ NonBlockingServer/src/com/yjh/SingleThreadBlockingServer.java
@@ -0,0 +1,30 @@
+package com.yjh;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class SingleThreadBlockingServer {
+	public static void main(String[] args) throws IOException {
+		ServerSocket ss = new ServerSocket(8080);
+		while(true) {
+			Socket s = ss.accept();
+			InputStream in = s.getInputStream();
+			OutputStream out = s.getOutputStream();
+			int data;
+			while((data = in.read()) != -1 ) {
+				out.write(transmogrify(data));
+			}
+//			in.transferTo(out);
+			out.close();
+			in.close();
+			s.close();
+		}
+		
+	}
+
+	private static int transmogrify(int data) {
+		return Character.isLetter(data) ? data ^ ' ' : data;
+	}
+}
