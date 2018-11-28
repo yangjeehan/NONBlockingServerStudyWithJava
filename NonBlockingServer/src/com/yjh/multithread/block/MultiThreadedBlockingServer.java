91e96d286b8afa8885d06f7398947a7bcf4e8046
양지한 <han@iacryl.com>
2018-11-28 14:19:26 +0900

    update multi-thread-blocking server

---
+++ NonBlockingServer/src/com/yjh/multithread/block/MultiThreadedBlockingServer.java
@@ -0,0 +1,44 @@
+package com.yjh.multithread.block;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.UncheckedIOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class MultiThreadedBlockingServer {
+	
+	public static void main(String[] args) throws IOException {
+		ServerSocket ss = new ServerSocket(8080);
+		while(true) {
+			Socket s = ss.accept();
+			handle(s);
+		}
+	
+	}
+
+	private static void handle(Socket s) throws IOException {
+		new Thread(() -> {
+			System.out.println("Connected to " + s);
+			try (
+				Socket temp = s;
+				InputStream in = s.getInputStream();
+				OutputStream out = s.getOutputStream();
+			)   {
+				int data;
+				while((data = in.read()) != -1 ) {
+					out.write(transmogrify(data));
+				}
+			} catch (IOException ex) {
+//				RuntimeException을 확장시킨 모양, 그래서 간단하게 죽는 원인이 될 수 있다.
+				throw new UncheckedIOException(ex);
+			} finally {
+				System.out.println("Disconnected from " + s );
+			}
+		}).start();
+	}
+	
+	private static int transmogrify(int data) {
+		return Character.isLetter(data) ? data ^ ' ' : data;
+	}
+}
--- NonBlockingServer/src/com/yjh/single/SingleThreadBlockingServer.java
+++ NonBlockingServer/src/com/yjh/single/SingleThreadBlockingServer.java
@@ -1,37 +1,29 @@
-package com.yjh;
+package com.yjh.single;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-public class MultiThreadedBlockingServer {
-	
+public class SingleThreadBlockingServer {
 	public static void main(String[] args) throws IOException {
 		ServerSocket ss = new ServerSocket(8080);
 		while(true) {
 			Socket s = ss.accept();
-			handle(s);
-		}
-	
-	}
-
-	private static void handle(Socket s) throws IOException {
-		System.out.println("Connected to " + s);
-		try (
-			Socket temp = s;
 			InputStream in = s.getInputStream();
 			OutputStream out = s.getOutputStream();
-		)   {
 			int data;
 			while((data = in.read()) != -1 ) {
 				out.write(transmogrify(data));
 			}
-		} finally {
-			System.out.println("Disconnected from " + s );
+//			in.transferTo(out);
+			out.close();
+			in.close();
+			s.close();
 		}
+		
 	}
-	
+
 	private static int transmogrify(int data) {
 		return Character.isLetter(data) ? data ^ ' ' : data;
 	}
